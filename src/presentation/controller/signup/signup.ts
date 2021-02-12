5ee454b2482eb8455ecd7db2c57f3f076b949a37
viniciusCanuto <vncscanuto@gmail.com>
2021-02-12 11:00:59 -0300

    refactor: move Validation to protocols folder

--- src/presentation/controller/signup/signup.ts
+++ src/presentation/controller/signup/signup.ts
@@ -1,6 +1,6 @@
 import { HttpResponse, HttpRequest, Controller, AddAccount } from './signup-protocols'
 import { badRequest, serveError, ok } from '../../helpers/http/http-helper'
-import { Validation } from '../../helpers/validators/validation'
+import { Validation } from '../../protocols/validation'
 
 export class SignUpController implements Controller {
   private readonly addAccount: AddAccount
