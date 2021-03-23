82f62aab0d228bcade4429c4199b1b0c9657f1ba
viniciusCanuto <vncscanuto@gmail.com>
2021-03-23 15:33:32 -0300

    test: ensure load surveys route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -70,5 +70,13 @@ describe('Survey Routes', () => {
         })
         .expect(204)
     })
+
+    describe('GET /surveys', () => {
+      test('Should return to 403 on load surveys without accessToken', async () => {
+        await request(app)
+          .get('/api/surveys')
+          .expect(403)
+      })
+    })
   })
 })
