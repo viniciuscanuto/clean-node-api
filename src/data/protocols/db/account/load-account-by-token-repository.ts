74b09c434c44762b0d1c3526e372369912548df7
Vinicius Canuto <vncscanuto@gmail.com>
2021-10-24 16:37:37 -0300

    refactor: add types to LoadAccountByEmail usecase

--- src/data/protocols/db/account/load-account-by-token-repository.ts
+++ src/data/protocols/db/account/load-account-by-token-repository.ts
@@ -1,9 +1,7 @@
-import { AccountModel } from '@/domain/models'
-
 export interface LoadAccountByTokenRepository {
   loadByToken (token: string, role?: string): Promise<LoadAccountByTokenRepository.Result>
 }
 
 export namespace LoadAccountByTokenRepository {
-  export type Result = AccountModel
+  export type Result = { id: string }
 }
