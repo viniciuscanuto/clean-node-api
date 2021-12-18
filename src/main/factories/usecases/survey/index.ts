d71ee7e8fc3ab5491026a7729ed49dfd69a58266
Vinicius Canuto <vncscanuto@gmail.com>
2021-12-18 19:28:24 -0300

    refactor: reorganize files

--- src/main/factories/usecases/survey/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './db-add-survey-factory'
-export * from './db-load-answers-by-survey-factory'
-export * from './db-check-survey-by-id-factory'
-export * from './db-load-surveys-factory'
